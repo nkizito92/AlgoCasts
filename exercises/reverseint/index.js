7d1d765f168e105d455f7103e891f32cac89970d
nkizito92 <nkizito57@gmail.com>
2020-07-14 22:20:03 -0500

    passed test

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+    let revs = n.toString().split("").reverse().join("")
+    return Number(parseInt(revs)) * Math.sign(n)
+}
 
 module.exports = reverseInt;
