bfbb6fc0b2b900d524d03f4eb38a1e5fbf422f24
nkizito92 <nkizito57@gmail.com>
2020-09-02 07:57:52 -0500

    completed assignment

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let vowelCount = 0
+    const vow = ["a", "e", "i", "o", "u"]
+    for (let eChar of str.toLowerCase()) {
+        if (vow.includes(eChar)) {
+            vowelCount++
+        } else ( 
+            
+            vowelCount * 0
+        )
+    }
+    return vowelCount
+}
 
 module.exports = vowels;
