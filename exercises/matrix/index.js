50f36d5460749be87fc6905b490ac0f95dceaf3e
nkizito92 <nkizito57@gmail.com>
2020-10-04 22:52:52 -0500

    Solved the matrix assignment

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,57 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = [];
+    for (let i = 0; i < n; i++) {
+        results.push([])
+    }
+    let counter = 1; 
+    let startColumn = 0;
+    let startRow = 0;
+    let endCol = n - 1
+    let endRow = n - 1;
+    let resultArrSet = (ele, num) => {
+        results[ele][num] = counter 
+        counter++
+    }
+    while(startColumn <= endCol && startRow <= endRow) {
+        // Top row
+        for(let i = startColumn; i <= endCol; i++){
+            // results[startRow][i] = counter;
+            // counter++;
+            resultArrSet(startRow, i)
+        }
+        startRow++;
+
+        // right column
+        for(let j = startRow; j <= endRow; j++){
+            // results[j][endCol]= counter;
+            // counter++;
+            resultArrSet(j, endCol)
+        }
+        endCol--;
+
+        // bottom row
+
+        for(let x = endCol; x >= startColumn; x--) {
+            // results[endRow][x] = counter;
+            // counter++
+            resultArrSet(endRow, x)
+        }
+        endRow--;
+
+        // start column 
+
+        for(let c = endRow; c >= startRow; c--){
+            // results[c][startColumn] = counter;
+            // counter++;
+            resultArrSet(c, startColumn)
+        }
+        startColumn++;
+    }
+   
+    return results;
+}
 
 module.exports = matrix;
